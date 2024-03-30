068a6044e684e4144c95cf1a7a74b2cef0a7c415
Mayank Pal <mayankpal9654@gmail.com>
2024-03-30 16:26:35 +0530

    get users for sidebar endpoint

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,12 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connecttoMogodb.js";
 import messageRoutes from "./routes/message.routes.js"
+import userRoutes from "./routes/user.routes.js"
 
 dotenv.config(); //it should be called before process.env.PORT or other
 
 const app = express();
 const PORT= process.env.PORT || 2000 ; //.env is environmental varable take value from env or value
-
-
 
 //middlewares 
 app.use(express.json()); //to parse the incoming request with json payload(from req.body)
@@ -20,12 +19,11 @@ app.use(cookieParser()); //to get cookies in routes
 
 app.use("/api/auth", authRoutes); // whenever page goes to something like /api/auth then authRouters will be called 
 app.use("/api/messages",messageRoutes);
-
+app.use("/api/users",userRoutes);
 
 // app.get('/', (req,res)=>{
 //   res.send("hii");
 // })
-
 
 app.listen(PORT, ()=>{
   connectToMongoDB();
