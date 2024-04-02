6bf8a52068449970f14005ed3d2553ed90c0ae3f
Mayank Pal <mayankpal9654@gmail.com>
2024-04-02 17:44:22 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -13,6 +14,8 @@ dotenv.config(); //it should be called before process.env.PORT or other
 
 const PORT= process.env.PORT || 2000 ; //.env is environmental varable take value from env or value
 
+const __dirname = path.resolve();
+
 //middlewares 
 app.use(express.json()); //to parse the incoming request with json payload(from req.body)
 app.use(cookieParser()); //to get cookies in routes
@@ -21,6 +24,12 @@ app.use(cookieParser()); //to get cookies in routes
 app.use("/api/auth", authRoutes); // whenever page goes to something like /api/auth then authRouters will be called 
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 // app.get('/', (req,res)=>{
 //   res.send("hii");
